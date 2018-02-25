e1c4bb7e9cdb00c5f45dc86af8d808773d86e456
Audrius Petrekis <audriusp@wix.com>
2018-02-25 19:09:24 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace fizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int number = 1; number < 31; number++) {
+                if(number % 3 == 0) {
+                    Console.Write("Fizz");
+                }
+
+                if(number % 5 == 0) {
+                    Console.Write("Buzz");
+                }
+                
+                if(number % 3 != 0 && number % 5 != 0) {
+                    Console.Write(number);
+                }
+                
+                Console.WriteLine();
+            }
+        }
+    }
+}
